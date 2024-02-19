08768072295bb128d027cc610fc54ebd41d73ebc
rolisingh <rolsi1803@gmail.com>
2024-02-19 23:07:45 +0530

    string operations

--- script.js
+++ script.js
@@ -3,13 +3,38 @@
 // console.log(name);
 // console.log(age);
 
-let greetings="hello";
-let name="roli";
-let message=greetings + " "+name;
-console.log(message);
-console.log(message.length);
-console.log(message[3]);
+// let greetings="hello";
+// let name="roli";
+// let message=greetings + " "+name;
+// console.log(message);
+// console.log(message.length);
+// console.log(message[3]);
 
 
-let substring = message.substring(0,4);
-console.log(substring);
+// let substring = message.substring(0,4);
+// console.log(substring);
+
+// const person={
+//     name:"roli",
+//     age:null
+// }
+
+// console.log(person.name);
+// console.log(person.age);
+
+// let x=3;
+// let y=-6;
+//  let sum=x*y;
+//  console.log(sum);
+
+//  let sqrtof=Math.sqrt(x);
+//  console.log(sqrtof);
+//  let abs=Math.abs(y);
+//  console.log(abs);
+
+let age=18;
+if(age >= 18 ){
+    console.log("you are old enough to vote.");
+}else{
+    console.log("you are not old enough to vote.");
+}
