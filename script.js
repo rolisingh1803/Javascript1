a8b13ed914cb63f77f0da3ff94a6a30e39f0938e
rolisingh <rolsi1803@gmail.com>
2024-02-19 22:42:53 +0530

    updates script file

---
+++ script.js
@@ -0,0 +1,15 @@
+// let name="roli";
+// let age=21;
+// console.log(name);
+// console.log(age);
+
+let greetings="hello";
+let name="roli";
+let message=greetings + " "+name;
+console.log(message);
+console.log(message.length);
+console.log(message[3]);
+
+
+let substring = message.substring(0,4);
+console.log(substring);
